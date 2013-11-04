397a155d38775a261a0cdf89c31a54730362c1fe
Pedram Beheshti <sacred.seven@yahoo.com>
2013-11-04 21:36:29 +0330

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Drop::Application.config.secret_token = '039a3edea2c4e9627798b8ee181ee02f4cadaeaaf4fef039c71f8466a65ac43dbc91cff9994935331b9f9970bcd3271c02a8e1f7e0fb73604e84f403fecedb01'
